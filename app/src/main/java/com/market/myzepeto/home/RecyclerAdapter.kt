3aca38a495605419d2ec024cc28c6996a3efcb96
oyunseong <gkrlsanjf@naver.com>
2022-11-01 14:49:08 +0900

    commit

--- app/src/main/java/com/market/myzepeto/home/RecyclerAdapter.kt
+++ app/src/main/java/com/market/myzepeto/home/RecyclerAdapter.kt
@@ -1,47 +1,117 @@
 package com.market.myzepeto.home
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.market.myzepeto.R
-import com.market.myzepeto.model.Header
+import com.market.myzepeto.databinding.ItemTypeFriendBinding
+import com.market.myzepeto.databinding.ItemTypeMenuBinding
+import com.market.myzepeto.databinding.ItemTypePeedBinding
+import com.market.myzepeto.model.*
 
-class RecyclerAdapter : RecyclerView.Adapter<RecyclerAdapter.HeaderViewHolder>() {
-    private val items = mutableListOf<Header>()
+class RecyclerAdapter<T>(list: ArrayList<T>) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+    private val items = list
 
-    inner class HeaderViewHolder constructor(parent: ViewGroup) : RecyclerView.ViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.)
-    )
-
-
-    override fun onBindViewHolder(holder: RecyclerAdapter, position: Int) {
-        val currentHeader = getItem(position)
+    override fun getItemViewType(position: Int): Int {
+        return items[position].type
     }
 
-    override fun getItemCount(): Int {
-        return items.size
-    }
-
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): RecyclerAdapter.HeaderViewHolder {
-        TODO("Not yet implemented")
-    }
-
-
-    companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<Header>() {
-            override fun areItemsTheSame(oldItem: Header, newItem: Header): Boolean {
-                return true
-//                return oldItem.title = newItem.title
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val view: View?
+        return when (viewType) {
+            menu_type -> {
+                view = LayoutInflater.from(parent.context).inflate(
+                    R.layout.item_type_menu,
+                    parent,
+                    false
+                )
+                PeedViewHolder(view)
+            }
+            peed_type -> {
+                view = LayoutInflater.from(parent.context).inflate(
+                    R.layout.item_type_peed,
+                    parent,
+                    false
+                )
+                PeedViewHolder(view)
+            }
+            friend_type -> {
+                view = LayoutInflater.from(parent.context).inflate(
+                    R.layout.item_type_friend,
+                    parent,
+                    false
+                )
+                FriendViewHolder(view)
             }
 
-            override fun areContentsTheSame(oldItem: Header, newItem: Header): Boolean {
-                return true
+            else -> {
+                view = LayoutInflater.from(parent.context).inflate(
+                    R.layout.item_type_booth,
+                    parent,
+                    false
+                )
+                BoothViewHolder(view)
             }
         }
     }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        when (items[position].type) {
+            menu_type -> {
+                (holder as MenuViewHolder).bind(Menu(items[position]))
+            }
+
+        }
+    }
+
+
     // https://hanyeop.tistory.com/247
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+    inner class MenuViewHolder(private val binding: View) :
+        RecyclerView.ViewHolder(binding) {
+        fun bind(item: Menu) {
+            binding.apply {
+//            icon.background = item.title
+//                title.text = item.title
+            }
+        }
+    }
+
+    inner class PeedViewHolder(private val binding: View) :
+        RecyclerView.ViewHolder(binding) {
+        fun bind(item: Peed) {
+            binding.apply {
+//            peedImg.setImageResource()
+//                peedTitle.text = item.title
+//                peedLikeCount.text = item.like_count.toString()
+            }
+        }
+    }
+
+    inner class FriendViewHolder(private val binding: View) :
+        RecyclerView.ViewHolder(binding) {
+        fun bind(item: Friend) {
+            binding.apply {
+//            peedImg.setImageResource()
+//            .text = item.title
+//            peedLikeCount.text = item.like_count.toString()
+            }
+        }
+    }
+
+    inner class BoothViewHolder(private val binding: View) :
+        RecyclerView.ViewHolder(binding) {
+        fun bind(item: Booth) {
+            binding.apply {
+//            peedImg.setImageResource()
+//                peedTitle.text = item.title
+//                peedLikeCount.text = item.like_count.toString()
+            }
+        }
+    }
 }
--- app/src/main/java/com/market/myzepeto/model/Header.kt
+++ app/src/main/java/com/market/myzepeto/model/Header.kt
@@ -1,16 +1,26 @@
 package com.market.myzepeto.model
 
+
+/**
+ * @param type {1 : Menu,
+ *             {2 : Peed,
+ *             {3 : Friend,
+ *             {4 : Booth
+ * */
 interface Header {
+    val type: Int
     val image: String
     val title: String
 }
 
 data class Menu(
+    override val type: Int,
     override val image: String,
     override val title: String,
 ) : Header
 
 data class Peed(
+    override val type: Int,
     override val image: String,
     override val title: String,
     val text: String,
@@ -18,11 +28,13 @@ data class Peed(
 ) : Header
 
 data class Friend(
+    override val type: Int,
     override val image: String,
     override val title: String,
 ) : Header
 
 data class Booth(
+    override val type: Int,
     override val image: String,
     override val title: String,
     val text: String
